3d6656fba0b413a6ac05d6734363810f2d901410
Anthony <ruffina@evms.edu>
2020-09-28 15:32:29 -0400

    particle updates

--- root-folder/pivoting-landing-page/pivoting.js
+++ root-folder/pivoting-landing-page/pivoting.js
@@ -4,6 +4,20 @@ window.onload = function() {
     maxParticles: 155,
     color: ['#4bacb8', '#4bacb8'],
     minDistance: 150,
-    connectParticles: true
+    connectParticles: true,
+    responsive: [
+      {
+        breakpoint: 900,
+        options: {
+          maxParticles: 100
+        }
+      },
+      {
+        breakpoint: 600,
+        options: {
+          maxParticles: 0
+        }
+      }
+    ] 
   });
 };
