3f009b641b7014272dfb844ffd726d67f7d7c925
Anthony <ruffina@evms.edu>
2020-10-16 09:22:40 -0400

    added carousel

--- root-folder/stories/basic-story/protection/basic-story.js
+++ root-folder/stories/basic-story/protection/basic-story.js
@@ -3,6 +3,32 @@
 // aniamte-left 
 // animate-right
 const slideElementsUp = document.querySelectorAll('.animate-vertical');
+const nextThumbnail = document.getElementById('nextThumbnail')
+const prevThumbnail = document.getElementById('prevThumbnail')
+const maskCarousel = document.getElementById('maskCarousel')
+const slideModal = document.getElementById('slideModal') 
+const steps = document.querySelectorAll('.basic-story__mask-steps__thumbnail-wrapper')
+const closeModal = document.getElementById('closeModal')
+const slides = document.querySelectorAll('.modal__slides')
+const slideOne = document.getElementById('slideOne')
+
+const modalSlides = {
+  slideOne: '../../../assets/slide-picture1@2x.jpg',
+  slideTwo: '../../../assets/slide-picture2@2x.jpg',
+  slideThree: '../../../assets/slide-picture3@2x.jpg',
+  slideFour: '../../../assets/slide-picture4@2x.jpg',
+  slideFive: '../../../assets/slide-picture5@2x.jpg',
+  slideFiveB: '../../../assets/slide-picture5b@2x.jpg',
+  slideSix: '../../../assets/slide-picture6@2x.jpg',
+  slideSeven: '../../../assets/slide-picture7@2x.jpg',
+  slideEight: '../../../assets/slide-picture8@2x.jpg',
+  slideEightB: '../../../assets/slide-picture8b@2x.jpg',
+  slideEightC: '../../../assets/slide-picture8c@2x.jpg',
+  slideNine: '../../../assets/slide-picture9@2x.jpg',
+  slideTen: '../../../assets/slide-picture10@2x.jpg',
+  slideEleven: '../../../assets/slide-picture11@2x.jpg'
+}
+
 
 const config = {
   threshold: [0.7]
@@ -53,3 +79,155 @@ observer = new IntersectionObserver((entries) => {
 slideElementsRight.forEach(element => {
   observer.observe(element);
 }, config);
+
+
+
+// logic for carousel
+let slideIndex = 1;
+
+showSteps(slideIndex);
+
+function showSteps(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    // slides[i].style.display = "none";
+    // console.log(slides[i])
+  }
+
+  // slides[slideIndex - 1].style.display = "block";
+  // steps[slideIndex - 1].classList.add('fadeIn')
+}
+
+function newSlide(n) {
+  showSteps((slideIndex += n));
+}
+
+function currentSlide(n) {
+  return showSteps(slideIndex = n);
+}
+
+function handleThumbnailClick() {
+  
+}
+
+handleThumbnailClick()
+
+function handleCurrentSlide() {
+  steps.forEach(step => {
+    step.addEventListener('click', function() {
+      let id = this.id
+      switch (id) {
+        case 'stepOne':
+          showModal()
+          showSlide(modalSlides.slideOne)
+          break;
+          
+        case 'stepTwo':
+          showModal()
+          showSlide(modalSlides.slideTwo)
+          break;
+
+        case 'stepThree':
+          showModal()
+          showSlide(modalSlides.slideThree)
+          break;
+
+        case 'stepFour':
+          showModal()
+          showSlide(modalSlides.slideFour)
+          break;
+
+        case 'stepFive':
+          showModal()
+          showSlide(modalSlides.slideFive)
+          break;
+
+        case 'stepFiveB':
+          showModal()
+          showSlide(modalSlides.slideFiveB)
+          break;
+
+        case 'stepSix':
+          showModal()
+          showSlide(modalSlides.slideSix)
+          break;
+
+        case 'stepSeven':
+          showModal()
+          showSlide(modalSlides.slideSeven)
+          break;
+
+        case 'stepEight':
+          showModal()
+          showSlide(modalSlides.slideEight)
+          break;
+
+        case 'stepEightB':
+          showModal()
+          showSlide(modalSlides.slideEightB)
+          break;
+
+        case 'stepEightC':
+          showModal()
+          showSlide(modalSlides.slideEightC)
+          break;
+
+        case 'stepNine':
+          showModal()
+          showSlide(modalSlides.slideNine)
+          break;
+
+        case 'stepTen':
+          showModal()
+          showSlide(modalSlides.slideTen)
+          break;
+
+        case 'stepEleven':
+          showModal()
+          showSlide(modalSlides.slideEleven)
+          break;
+
+        default:
+          break;
+      }
+    })
+  });
+}
+
+handleCurrentSlide()
+
+function showModal() {
+  slideModal.style.display = 'block'
+  // html.style.overflow = "hidden";
+}
+
+function hideModal() {
+  slideModal.style.display = 'none'
+}
+
+function showSlide(slide) {
+  slide.style.display = 'block'
+
+  // if(!slide) {
+  //   console.log('not')
+  //   slide.style.display = 'none'
+  // }
+}
+
+const scrollDistance = 500;
+
+nextThumbnail.addEventListener('click', function() {
+  maskCarousel.scrollLeft += scrollDistance
+})
+
+prevThumbnail.addEventListener('click', function() {
+  maskCarousel.scrollLeft -= scrollDistance
+})
+
+closeModal.addEventListener('click', hideModal)
