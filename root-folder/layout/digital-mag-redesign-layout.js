8bc61ac1c18db0789b17aa072eb62c4ed3141888
Anthony <ruffina@evms.edu>
2020-10-30 12:17:58 -0400

    updates

--- root-folder/layout/digital-mag-redesign-layout.js
+++ root-folder/layout/digital-mag-redesign-layout.js
@@ -22,18 +22,7 @@ const pivotingMenu = document.getElementById('pivotingMenu')
 const categoryPivoting = document.getElementById('categoryPivoting')
 const expandPivotingList = document.getElementById('expandPivotingList')
 const pivotingList = document.getElementById('pivotingList')
-const prevThumbnail = document.getElementById('prevThumbnail')
-const nextThumbnail = document.getElementById('nextThumbnail')
-const storyCarousel = document.getElementById('storyCarousel')
 
-const scrollDistance = 500;
-nextThumbnail.addEventListener('click', function() {
-  storyCarousel.scrollLeft += scrollDistance
-})
-
-prevThumbnail.addEventListener('click', function() {
-  storyCarousel.scrollLeft -= scrollDistance
-})
 
 
 categoryFeatures.addEventListener('click', function() {
